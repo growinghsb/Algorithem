31d09119409a1df0917104336b6a0cc9a624dec4
growinghsb <seungbeombm@gmail.com>
2022-06-14 11:20:17 +0900

    [ADD] 2439번_별찍기-2 finish

---
+++ 2439번_별찍기-2/2439번_별찍기-2.cpp
@@ -0,0 +1,26 @@
+﻿#pragma warning(disable : 4996)
+#include <iostream>
+
+int main()
+{
+	int count;
+	scanf("%d", &count);
+
+	int star = 1;
+	for (int i = 0; i < count; ++i)
+	{
+		for (int j = 0; j < count; ++j)
+		{
+			if (count - star <= j)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+		++star;
+	}
+}

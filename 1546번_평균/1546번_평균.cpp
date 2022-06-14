bca4a244c62c4c8bc6083a32c5596777a8eff0b6
growinghsb <seungbeombm@gmail.com>
2022-06-14 11:07:50 +0900

    [ADD] 1546_평균 clear

---
+++ 1546번_평균/1546번_평균.cpp
@@ -0,0 +1,33 @@
+﻿#pragma warning (disable : 4996)
+#include <iostream>
+
+int main()
+{
+	int count;
+	scanf("%d", &count);
+
+	int subject[1000];
+	int max = 0;
+	int curIdx = 0;
+	for (int i = 0; i < count; ++i)
+	{
+		scanf("%d", &subject[i]);
+		fflush(stdin);
+
+		if (max < subject[i])
+		{
+			max = subject[i];
+		}
+		++curIdx;
+	}
+
+	float sum = 0;
+	for (int i = 0; i < curIdx; ++i)
+	{
+		sum += ((float(subject[i]) / max) * 100.f);
+	}
+
+	printf("%f", sum / curIdx);
+
+	return 0;
+}

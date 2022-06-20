900bc6309d8289ad009c5e5451e959310f3391b3
growinghsb <seungbeombm@gmail.com>
2022-06-20 13:25:52 +0900

    [ADD] 220620 one solve finish

---
+++ 2908번_상수/2908번_상수.cpp
@@ -0,0 +1,33 @@
+﻿#pragma warning (disable : 4996)
+#include <iostream>
+
+int main()
+{
+	int num1, num2, tmp;
+	scanf("%d %d", &num1, &num2);
+
+	int num1Result = 0;
+	int num2Result = 0;
+
+	num1Result = num1 * 0.01;
+	num1 -= num1Result * 100;
+
+	tmp = num1 * 0.1;
+	num1 -= tmp * 10;
+	num1Result += tmp * 10;
+
+	num1Result += num1 * 100;
+
+	num2Result = num2 * 0.01;
+	num2 -= num2Result * 100;
+
+	tmp = num2 * 0.1;
+	num2 -= tmp * 10;
+	num2Result += tmp * 10;
+
+	num2Result += num2 * 100;
+
+	printf("%d", num1Result > num2Result ? num1Result : num2Result);
+
+	return 0;
+}

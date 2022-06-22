ab928ad5c41d3e699eaf8c550f33160571af582d
growinghsb <seungbeombm@gmail.com>
2022-06-22 11:27:55 +0900

    [ADD] 220622 2solve finish

---
+++ 2920번_음계/2920번_음계.cpp
@@ -0,0 +1,28 @@
+﻿#pragma warning (disable : 4996)
+#include <iostream>
+
+int main()
+{
+	int nums[8];
+	scanf("%d %d %d %d %d %d %d %d", &nums[0], &nums[1], &nums[2], &nums[3], &nums[4], &nums[5], &nums[6], &nums[7]);
+
+	if (nums[0] != 1 && nums[0] != 8)
+	{
+		printf("mixed");
+		return 0;
+	}
+
+	for (int i = 0; i < 7; ++i)
+	{
+		int result = abs(nums[i] - nums[i + 1]);
+		if (result != 1)
+		{
+			printf("mixed");
+			return 0;
+		}
+	}
+
+	nums[0] == 1 ? printf("ascending") : printf("descending");
+
+	return 0;
+}
---
+++ 3052번_나머지/3052번_나머지.cpp
@@ -0,0 +1,41 @@
+﻿#pragma warning (disable : 4996)
+#include <iostream>
+
+int main()
+{
+	int remainder[10] = {};
+	int idx = 0;
+
+	int num, result;
+	bool flag;
+	for (int i = 0; i < 10; ++i)
+	{
+		flag = true;
+
+		scanf("%d", &num);
+		result = num % 42;
+
+		if (idx == 0)
+		{
+			remainder[idx++] = result;
+			continue;
+		}
+
+		for (int j = 0; j < idx; ++j)
+		{
+			if (result == remainder[j])
+			{
+				flag = false;
+				break;
+			}
+		}
+
+		if (flag)
+		{
+			remainder[idx++] = result;
+		}
+	}
+	printf("%d", idx);
+
+	return 0;
+}

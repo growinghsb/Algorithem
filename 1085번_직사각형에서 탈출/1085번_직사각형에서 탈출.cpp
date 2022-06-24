928559ff34c72dd73ba535f26ff72f38f6126595
growinghsb <seungbeombm@gmail.com>
2022-06-24 12:34:34 +0900

    [ADD] 220624 1 solve finish

---
+++ 1085번_직사각형에서 탈출/1085번_직사각형에서 탈출.cpp
@@ -0,0 +1,19 @@
+﻿#pragma warning (disable : 4996)
+#include <iostream>
+
+int main()
+{
+	int x, y, w, h;
+	scanf("%d %d %d %d", &x, &y, &w, &h);
+
+	int resultX = abs(x - w);
+	int resultY = abs(y - h);
+
+	int inputMin, calculMin;
+	inputMin = x <= y ? x : y;
+	calculMin = resultX <= resultY ? resultX : resultY;
+
+	printf("%d", inputMin <= calculMin ? inputMin : calculMin);
+
+	return 0;
+}

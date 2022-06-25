0f6a63f44739cfc5bc95e387536199981fd48e7b
growinghsb <seungbeombm@gmail.com>
2022-06-25 12:45:43 +0900

    [ADD] 220625 2 solve finish

---
+++ 10250번_ACM호텔/10250번_ACM호텔.cpp
@@ -0,0 +1,30 @@
+﻿#pragma warning (disable : 4996)
+#include <iostream>
+
+int main()
+{
+	int count;
+	scanf("%d", &count);
+
+	int h, w, n;
+	for (int i = 0; i < count; ++i)
+	{
+		scanf("%d %d %d", &h, &w, &n);
+
+		int floor = n % h;
+
+		if (floor == 0)
+		{
+			int noh = n / h;
+			printf("%d\n", h * 100 + noh);
+		}
+		else
+		{
+			int noh = (n / h) + 1;
+			printf("%d\n", floor * 100 + noh);
+		}
+
+	}
+
+	return 0;
+}
---
+++ 4153번_직각삼각형/4153번_직각삼각형.cpp
@@ -0,0 +1,33 @@
+﻿#pragma warning (disable : 4996)
+#include <iostream>
+
+int main()
+{
+	int sum = 0;
+	int num1, num2, num3;
+
+	for (;;)
+	{
+		scanf("%d %d %d", &num1, &num2, &num3);
+		if (num1 == 0 && num2 == 0 && num3 == 0)
+		{
+			break;
+		}
+
+		num1 *= num1;
+		num2 *= num2;
+		num3 *= num3;
+
+		if (num1 + num2 == num3 ||
+			num2 + num3 == num1 ||
+			num3 + num1 == num2)
+		{
+			printf("%s\n", "right");
+		}
+		else
+		{
+			printf("%s\n", "wrong");
+		}
+	}
+	return 0;
+}

f46cc16cdcf34602ca3490a3e402f4e6acfa2478
growinghsb <seungbeombm@gmail.com>
2022-06-27 14:50:59 +0900

    [ADD] 2231 ing

---
+++ 2231번_분해합/2231번_분해합.cpp
@@ -0,0 +1,14 @@
+﻿#pragma warning (disable : 4996)
+#include <iostream>
+
+int main()
+{
+	int num;
+	scanf("%d", &num);
+
+	int remainder = num % 10 - 9;
+	int digitSum = remainder + 9 - remainder;
+
+
+	return 0;
+}

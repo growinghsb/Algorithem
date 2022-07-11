b787c1b5ac9cfc0faf23e5c0e121893be16e5b7d
growinghsb <seungbeombm@gmail.com>
2022-07-11 11:24:18 +0900

    [ADD] 2 solve infrun

---
+++ 13번_/13번_가장 많이 사용된 자릿수.cpp
@@ -0,0 +1,40 @@
+﻿#pragma warning (disable : 4996)
+#include <iostream>
+
+int strLen(const char* pCh)
+{
+	int count = 0;
+
+	while (*pCh++ != '\0')
+	{
+		++count;
+	}
+	return count;
+}
+
+int main()
+{
+	int count[10] = {};
+	char input[101] = {};
+
+	scanf("%s", input);
+
+	int len = strLen(input);
+	for (int i = 0; i < len; ++i)
+	{
+		count[input[i] - 48]++;
+	}
+
+	int max = 0;
+	for (int i = 0; i < 9; ++i)
+	{
+		if (count[max] < count[i + 1] || count[max] == count[i + 1])
+		{
+			max = i + 1;
+		}
+	}
+
+	printf("%d", max);
+
+	return 0;
+}
---
+++ 14번_뒤집은 소수/14번_뒤집은 소수.cpp
@@ -0,0 +1,71 @@
+﻿#pragma warning(disable : 4996)
+#include <iostream>
+
+int len(const char* pCh)
+{
+	int count = 0;
+	while (*pCh++ != '\0')
+	{
+		++count;
+	}
+	return count;
+}
+
+int reverse(int x)
+{
+	char buf[7];
+	itoa(x, buf, 10);
+
+	int length = len(buf);
+
+	char* start = buf;
+	char* end = buf + length - 1;
+
+	while (start < end)
+	{
+		char tmp = *start;
+		*start++ = *end;
+		*end-- = tmp;
+	}
+
+	return atoi(buf);
+}
+
+bool isPrime(int x)
+{
+	for (int i = 2; i < x; ++i)
+	{
+		if (x % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int count;
+	scanf("%d", &count);
+	getchar();
+
+	int* input = new int[count];
+	for (int i = 0; i < count; ++i) 
+	{
+		scanf("%d", input + i);
+		getchar();
+	}
+
+	for (int i = 0; i < count; ++i)
+	{
+		int reverseValue = reverse(*(input + i));
+
+		if (isPrime(reverseValue))
+		{
+			printf("%d ", reverseValue);
+		}
+	}
+
+	delete[] input;
+	return 0;
+}
